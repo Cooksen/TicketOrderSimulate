a05766617a997d788b0ddc880717b51a302cae0b
Cooksen <73888718+Cooksen@users.noreply.github.com>
2021-06-27 18:53:40 +0800

    Update car5all.java

--- oop_final/src/car5all.java
+++ oop_final/src/car5all.java
@@ -18,7 +18,7 @@ public class car5all {
 	try {
 		Class.forName("com.mysql.jdbc.Driver");
 		long start = System.currentTimeMillis();
-		conn = DriverManager.getConnection("jdbc:mysql://kaycloud.i234.me:3306/sen_oop?useSSL=false", "senchao", "senchao");
+		conn = DriverManager.getConnection("jdbc:mysql://kaycloud.i234.me:3306/sen_oop?useSSL=false", "senchao", "password");
 		
 		System.out.println(conn);
 
